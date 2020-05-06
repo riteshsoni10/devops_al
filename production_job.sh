a4088e06674355dd5184321e1d6f595772514e28
riteshsoni10 <32491007+riteshsoni10@users.noreply.github.com>
2020-05-07 04:15:11 +0530

    Update production_job.sh

--- production_job.sh
+++ production_job.sh
@@ -19,17 +19,17 @@ if ! sudo systemctl status docker >/dev/null ; then
 fi
 
 # Checking whether production is already live 
-if sudo docker ps  | grep "prod_web"; then
+if sudo docker ps  | grep "prod_web" >/dev/null; then
 	echo "Production Container already running"
 
-elif sudo docker ps -a | grep "prod_web"; then
+elif sudo docker ps -a | grep "prod_web" >/dev/null; then
 	echo "Production container was in stopped state"
     	echo "Starting Production"
     	sudo docker start prod_web
 
 else
 	echo "Production is not live. Going on Live now"
-	if ! sudo docker network ls | grep "prod_application"; then
+	if ! sudo docker network ls | grep "prod_application" >/dev/null; then
     		sudo docker network create --subnet 10.100.20.0/24  --driver bridge prod_application
     	fi
 	sudo docker run -dit --network prod_application -v /opt/prod/:/usr/local/apache2/htdocs/ --name prod_web -p 80:80 httpd:alpine
