d43363373627752bd33aed617f3da608df4a5167
riteshsoni10 <32491007+riteshsoni10@users.noreply.github.com>
2020-05-07 08:29:51 +0530

    Update test_environment_job.sh

--- test_environment_job.sh
+++ test_environment_job.sh
@@ -17,19 +17,19 @@ if ! sudo systemctl status docker >/dev/null ; then
 fi
 
 # Checking whether production is already live 
-if sudo docker ps  | grep "test_env_web" >/dev/null; then
-	echo "Test Environment Container already running"
-
-elif sudo docker ps -a | grep "test_env_web" >/dev/null; then
-	echo "Test Environment container was in stopped state"
-    	echo "Starting Test Environment"
-    	sudo docker start test_env_web
+if sudo docker ps -a | grep "test_env_web" >/dev/null; then
+    echo "Test Environment Container already present"
+    echo "Removing Test Environment"
+    sudo docker rm -f test_env_web
 
 else
 	echo "Test Environment is not live. Going on Live now"
 	if ! sudo docker network ls | grep "test_env_application" >/dev/null; then
     		sudo docker network create --subnet 10.170.12.0/24  --driver bridge test_env_application
     	fi
-	sudo docker run -dit --network test_env_application -v /opt/test-env/:/usr/local/apache2/htdocs/ --name test_env_web -p 8080:80 httpd:alpine
 
 fi
+
+echo "Initialising Test Container"
+sudo docker run -dit --network test_env_application -v /opt/test-env/:/usr/local/apache2/htdocs/ --name test_env_web -p 8085:80 httpd:alpine
+
